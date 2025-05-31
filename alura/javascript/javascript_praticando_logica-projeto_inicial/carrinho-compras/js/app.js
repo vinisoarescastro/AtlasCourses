b9500dca9d0f52173a841147880199ceaf8c9f02
Vinicius Soares <vinisoarescastro@gmail.com>
2025-05-31 08:57:49 -0300

    refresh

---
+++ alura/javascript/javascript_praticando_logica-projeto_inicial/carrinho-compras/js/app.js
@@ -0,0 +1,7 @@
+function  adicionar(){
+    let produto = document.getElementById('produto');
+    let quantidade = document.getElementById('quantidade');
+
+    alert(produto.value);
+    alert(quantidade.value);
+}
