f64f2bedeb2274e56707250f20409705e39e56a1
Vinicius Soares <vinisoarescastro@gmail.com>
2025-06-06 17:32:42 -0300

    refresh

--- alura/javascript/javascript_array/dividir-array.js
+++ alura/javascript/javascript_array/dividir-array.js
@@ -1,7 +1,7 @@
 const listaEstudantes = ['João', 'Juliana', 'Ana', 'Caio', 'Lara', 'Marjorie', 'Guilherme', 'Aline', 'Fabiana', 'André', 'Carlos', 'Paulo', 'Bia', 'Vivian', 'Isabela', 'Vinicius', 'Renan', 'Renata', 'Daisy', 'Camilo'];
 
-const sala1 = listaEstudantes.slice(0, listaEstudantes.length/2);
-const sala2 = listaEstudantes.slice(listaEstudantes.length/2)
+const sala1 = listaEstudantes.slice(0, listaEstudantes.length/2); // Divide a lista em duas partes
+const sala2 = listaEstudantes.slice(listaEstudantes.length/2); // Pega a segunda metade da lista, só colocar o começo da lista
 
 console.log(sala1);
 console.log(sala2);
